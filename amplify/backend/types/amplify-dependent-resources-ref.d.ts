0b88e063c5fa6bc6b85c5fcb62086b2256a2ff61
sittinprettyingeorgia <mitchellblake29@aol.com>
2023-01-01 00:16:48 -0800

    remove auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -10,8 +10,6 @@ export type AmplifyDependentResourcesAttributes = {
         "myapplicationsecretaryamplify": {
             "IdentityPoolId": "string",
             "IdentityPoolName": "string",
-            "HostedUIDomain": "string",
-            "OAuthMetadata": "string",
             "UserPoolId": "string",
             "UserPoolArn": "string",
             "UserPoolName": "string",
