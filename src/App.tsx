3e4dd8920030e99b3ebc9d4192c84cbef8180406
sittinprettyingeorgia <mitchellblake29@aol.com>
2023-01-01 01:53:32 -0800

    updated frontend

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,42 @@ import React, { useEffect, useState } from 'react';
 import { Amplify, Auth, Hub } from 'aws-amplify';
 import { CognitoHostedUIIdentityProvider } from '@aws-amplify/auth';
 import awsconfig from './aws-exports';
+//Check if you are in localhost or production
 
+const isLocalhost = Boolean(
+
+  window.location.hostname === 'localhost' ||
+
+    // [::1] is the IPv6 localhost address.
+
+    window.location.hostname === '[::1]' ||
+
+    // 127.0.0.0/8 are considered localhost for IPv4.
+
+    window.location.hostname.match(
+
+      /^127(?:.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+
+    )
+
+);
+
+const signInURI = awsconfig.oauth.redirectSignIn.split(',')
+const signOutURI = awsconfig.oauth.redirectSignOut.split(',')
+
+if (isLocalhost) {
+  awsconfig.oauth.redirectSignIn = signInURI[0]
+  awsconfig.oauth.redirectSignOut = signOutURI[0]
+} else if (window.location.hostname === 'https://www.myapplicationsecretary.com') {
+  awsconfig.oauth.redirectSignIn = signInURI[1]
+  awsconfig.oauth.redirectSignOut = signOutURI[1]
+} else {
+  console.log('This is not possible');
+}
+
+//Check if you are in localhost or production
+
+//Then Configure Resources
 Amplify.configure(awsconfig);
 
 function App() {
