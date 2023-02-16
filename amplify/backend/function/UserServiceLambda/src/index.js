a18baf9126b3189b9759af4fd1cdde1c4e2eb828
sittinprettyingeorgia <mitchellblake29@aol.com>
2023-02-15 21:24:37 -0800

    added await

--- amplify/backend/function/UserServiceLambda/src/index.js
+++ amplify/backend/function/UserServiceLambda/src/index.js
@@ -9,7 +9,7 @@ const server = awsServerlessExpress.createServer(app);
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
-exports.handler = (event, context) => {
+exports.handler = async(event, context) => {
   console.log(`EVENT: ${JSON.stringify(event)}`);
-  return awsServerlessExpress.proxy(server, event, context, 'PROMISE').promise;
+  return await awsServerlessExpress.proxy(server, event, context, 'PROMISE').promise;
 };
