ab94fb3d479d750cbfcbd586010ee91e23801bdd
sittinprettyingeorgia <mitchellblake29@aol.com>
2023-02-17 01:39:56 -0800

    moved head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
 import type { AppProps } from 'next/app';
 import '@aws-amplify/ui-react/styles.css';
 import '@/styles.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta name='theme-color' content='#000000' />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
