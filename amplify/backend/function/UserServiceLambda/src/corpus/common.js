42205f761bfacc4648ca732f500967cd727722b5
sittinprettyingeorgia <mitchellblake29@aol.com>
2023-03-08 00:27:50 -0800

    more questions

--- amplify/backend/function/UserServiceLambda/src/corpus/common.js
+++ amplify/backend/function/UserServiceLambda/src/corpus/common.js
@@ -1,32 +1,35 @@
 
-const corpus = {
-  "name": "QuestionsAndAnswers",
+module.exports.commonCorpus = {
+  "name": "common",
   "locale": "en-US",
   "data": [
   {
-    "intent": "question.age",
+    "intent": "question.YesLeaning",
     "utterances": [
-      "Are you at least 18 years of age or older?"
+      "Are you at least 18 years of age or older?",
+      "Do you speak Fluent English?"
+      "Are you willing to undergo a pre-employment background check?",
+      "Are you a US Citizen (naturalized or U.S. Born)",
+      "Because this supports the Federal Government, are you a US Citizen",
+      "Do you have a legal right to work for any employer in the United States?",
+      "Are you authorized to work in the United States",
+      "Are you legally eligible to work in the United States?",
+      "Do you have the right to work in the US",
+      "Are you able to obtain and maintain a security clearance?",
+      "Please indicate if you agree to be contacted by sms text",
+      "Yes, I agree to be contacted by text messages",
+      "I consent to the hiring team of this job contacting me via text message",
+      "Voluntary Self-Identification of Disability\n\n We are a federal contractor or subcontractor required by law to provide equal employment opportunity to qualified people with disabilities. We are also required to measure our progress toward having at least 7% of our workforce be individuals with disabilities. To do this, we must ask applicants and employees if they have a disability or have ever had a disability.  Because a person may become disabled at any time, we ask all of our employees to update their information at least every five years. \n\n Please follow this link to get more information: https://tinyurl.com/y7hbuntb \n",
+      "I certify that answers given herein are true, accurate, and complete to the best of my knowledge. I authorize investigation into all statements I have made on this Form as may be necessary for reaching an employment decision. In the event I am employed, I understand that any false or misleading information I knowingly provided herein or in subsequent interviews may result in discharge and/or legal action. I understand that this Career History Form is not the sole basis from which a hiring decision has been made, and understand that this Form will be used in conjunction with a series of interviews and assessments to determine my suitability for the role to which I am applying.\nSignature"
+      "As an equal opportunity employer, we hire without consideration to race, religion, creed, color, national origin, age, gender, sexual orientation, marital status, veteran status, or disability. We invite you to complete the optional self-identification fields below used for compliance with government regulations and record-keeping guidelines.\nGender"
+
     ],
-    "answers": []
-  },
-  {
-    "intent": "question.backgroundCheck",
-    "utterances": [
-      "Are you willing to undergo a pre-employment background check?"
-    ],
-    "answers": []
+    "answers": ["yes", "agree"]
   },
   {
     "intent": "question.citizenship",
     "utterances": [
-      "Are you authorized to work in the United States",
-      "Are you legally eligible to work in the United States?",
-      "Do you have the right to work in the US",
       "What is your citizenship",
-      "Are you a US Citizen (naturalized or U.S. Born)",
-      "Because this supports the Federal Government, are you a US Citizen",
-      "Do you have a legal right to work for any employer in the United States?"
     ],
     "answers": []
   },
@@ -45,18 +48,8 @@ const corpus = {
     "answers": []
   },
   {
-    "intent": "question.clearance",
-    "utterances": [
-      "Are you able to obtain and maintain a security clearance?"
-    ],
-    "answers": []
-  },
-  {
     "intent": "question.contact",
     "utterances": [
-      "Please indicate if you agree to be contacted by sms text",
-      "Yes, I agree to be contacted by text messages",
-      "I consent to the hiring team of this job contacting me via text message",
       "What is your preferred method of contact?"
     ],
     "answers": []
@@ -65,13 +58,6 @@ const corpus = {
     "intent": "question.date",
     "utterances": [
       "Date"
-    ],
-    "answers": []
-  },
-  {
-    "intent": "question.disability",
-    "utterances": [
-      "Voluntary Self-Identification of Disability\n\n We are a federal contractor or subcontractor required by law to provide equal employment opportunity to qualified people with disabilities. We are also required to measure our progress toward having at least 7% of our workforce be individuals with disabilities. To do this, we must ask applicants and employees if they have a disability or have ever had a disability.  Because a person may become disabled at any time, we ask all of our employees to update their information at least every five years. \n\n Please follow this link to get more information: https://tinyurl.com/y7hbuntb \n"
     ],
     "answers": []
   },
@@ -100,21 +86,6 @@ const corpus = {
       "Earliest available"
     ],
     "answers": []
-  },
-  {
-    "intent": "question.ethnicity",
-    "utterances": [
-      "Ethnicity"
-    ],
-    "answers": []
-  },
-  {
-    "intent": "question.gender",
-    "utterances": [
-      "As an equal opportunity employer, we hire without consideration to race, religion, creed, color, national origin, age, gender, sexual orientation, marital status, veteran status, or disability. We invite you to complete the optional self-identification fields below used for compliance with government regulations and record-keeping guidelines.\nGender"
-    ],
-    "answers": [
-    ]
   },
   {
     "intent": "question.graduateYear",
@@ -153,7 +124,8 @@ const corpus = {
     "utterances": [
       "race/ethnicity",
       "race or ethnicity",
-      "Ethnic Background"
+      "Ethnic Background",
+      "Ethnicity"
     ],
     "answers": [
     ]
@@ -171,7 +143,7 @@ const corpus = {
     "intent": "question.salary",
     "utterances": [
       "Salary requirements",
-      "compensation",
+      "compensation requirements",
       "Desired Pay",
       "Desired Annual Salary"
     ],
@@ -182,14 +154,6 @@ const corpus = {
     "intent": "question.school",
     "utterances": [
       "School name"
-    ],
-    "answers": [
-    ]
-  },
-  {
-    "intent": "question.signature",
-    "utterances": [
-      "I certify that answers given herein are true, accurate, and complete to the best of my knowledge. I authorize investigation into all statements I have made on this Form as may be necessary for reaching an employment decision. In the event I am employed, I understand that any false or misleading information I knowingly provided herein or in subsequent interviews may result in discharge and/or legal action. I understand that this Career History Form is not the sole basis from which a hiring decision has been made, and understand that this Form will be used in conjunction with a series of interviews and assessments to determine my suitability for the role to which I am applying.\nSignature"
     ],
     "answers": [
     ]
@@ -234,4 +198,3 @@ const corpus = {
     "answers": []
   }]
 };
-module.exports.corpus = corpus;
