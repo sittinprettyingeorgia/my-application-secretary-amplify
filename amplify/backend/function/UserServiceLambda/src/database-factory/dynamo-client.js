fab4b2add757e5a8333803dc75911f9b0c9c49ad
sittinprettyingeorgia <mitchellblake29@aol.com>
2023-03-16 18:07:30 -0700

    updated jobLinks to update user

--- amplify/backend/function/UserServiceLambda/src/database-factory/dynamo-client.js
+++ amplify/backend/function/UserServiceLambda/src/database-factory/dynamo-client.js
@@ -1,96 +1,104 @@
-const { DynamoDBClient, GetItemCommand, PutItemCommand } = require('@aws-sdk/client-dynamodb');
+const {
+  DynamoDBClient,
+  GetItemCommand,
+  PutItemCommand,
+  UpdateItemCommand
+} = require('@aws-sdk/client-dynamodb');
 const { DynamoDBDocument } = require('@aws-sdk/lib-dynamodb');
 const { unmarshall, marshall } = require('@aws-sdk/util-dynamodb');
 
 const getDynamoClient = () => {
-    try {
-        let dynamoClient;
-        const marshallOptions = {
-            // Whether to automatically convert empty strings, blobs, and sets to `null`.
-            convertEmptyValues: true, // false, by default.
-        };
-        const unmarshallOptions = {
-            // Whether to return numbers as a string instead of converting them to native JavaScript numbers.
-            wrapNumbers: false, // false, by default.
-        
-            // Here we specify that the 'listAttr' field is a List type
-            // with elements of type 'S' (String)
-            typeConverter: {
-                fromDb: (attributeValue) =>
-                attributeValue.L.map((value) => value.S),
-            },
-        };
-        const translateConfig = { marshallOptions, unmarshallOptions };
+  try {
+    let dynamoClient;
+    const marshallOptions = {
+      // Whether to automatically convert empty strings, blobs, and sets to `null`.
+      convertEmptyValues: true // false, by default.
+    };
+    const unmarshallOptions = {
+      // Whether to return numbers as a string instead of converting them to native JavaScript numbers.
+      wrapNumbers: false, // false, by default.
 
-        let dynamoDB = new DynamoDBClient({ region: process.env.REGION });
-        dynamoClient = DynamoDBDocument.from(
-            dynamoDB, translateConfig
-        );
+      // Here we specify that the 'listAttr' field is a List type
+      // with elements of type 'S' (String)
+      typeConverter: {
+        fromDb: attributeValue => attributeValue.L.map(value => value.S)
+      }
+    };
+    const translateConfig = { marshallOptions, unmarshallOptions };
 
-        return dynamoClient;
-    } catch (e) {
-        console.log(e);
-    }
+    let dynamoDB = new DynamoDBClient({ region: process.env.REGION });
+    dynamoClient = DynamoDBDocument.from(dynamoDB, translateConfig);
+
+    return dynamoClient;
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 class DynamoUtil {
-    dynamoClient;
+  dynamoClient;
 
-    constructor(dynamoClient) {
-        this.dynamoClient = dynamoClient;
-        this.lastRefillTime = Date.now();
+  constructor(dynamoClient) {
+    this.dynamoClient = dynamoClient;
+    this.lastRefillTime = Date.now();
+  }
+
+  async query(action, Username) {
+    let result;
+
+    switch (action) {
+      case 'getUser':
+        result = await this.getItem(Username);
+        break;
     }
 
-    async query(action, Username){
-        let result;
+    return result;
+  }
 
-        switch(action) {
-            case 'getUser':
-               result = await this.getItem(Username);
-               break; 
-        }
-        
-        return result;
+  async getItem(
+    identifier,
+    TableName = process.env.API_MYAPPLICATIONSECRETARYAMPLIFY_USERTABLE_NAME
+  ) {
+    try {
+      if (!identifier) {
+        throw new Error('The key could not be found');
+      }
+
+      const Key = {
+        identifier: { S: identifier }
+      };
+
+      const params = { TableName, Key };
+      const result = await this.dynamoClient.send(new GetItemCommand(params));
+
+      if (result?.Item) {
+        return unmarshall(result.Item);
+      }
+
+      return null;
+    } catch (e) {
+      console.log(e);
+      return 'There was an error retrieving the item';
     }
+  }
 
-    async getItem(identifier, TableName=process.env.API_MYAPPLICATIONSECRETARYAMPLIFY_USERTABLE_NAME) {
-        try{;
-            if(!identifier){
-                throw new Error('The key could not be found');
-            }
+  async putItem(
+    item,
+    TableName = process.env.API_MYAPPLICATIONSECRETARYAMPLIFY_USERTABLE_NAME
+  ) {
+    try {
+      if (!item) {
+        throw new Error('The item could not be found');
+      }
 
-            const Key = {
-                'identifier': {S: identifier}
-            }
-
-            const params = { TableName, Key };
-            const result = await this.dynamoClient.send(new GetItemCommand(params));
-
-            if(result?.Item){
-                return unmarshall(result.Item)
-            }
-
-            return null;
-        }catch(e){
-            console.log(e);
-            return 'There was an error retrieving the item';
-        }
+      const Item = marshall(item);
+      const params = { TableName, Item };
+      await this.dynamoClient.send(new PutItemCommand(params));
+    } catch (e) {
+      console.log(e);
+      return 'There was an error retrieving the user';
     }
-
-    async putItem(item, TableName) {
-        try{;
-            if(!item){
-                throw new Error('The item could not be found');
-            }
-
-            const Item = marshall(item);
-            const params = { TableName, Item };
-            await this.dynamoClient.send(new PutItemCommand(params));
-        }catch(e){
-            console.log(e);
-            return 'There was an error retrieving the user';
-        }
-    }
+  }
 }
 
 const dynamoClient = getDynamoClient();
--- amplify/backend/function/UserServiceLambda/src/routes/user.js
+++ amplify/backend/function/UserServiceLambda/src/routes/user.js
@@ -1,45 +1,48 @@
 const express = require('express');
 const router = express.Router();
-const {dynamo} = require('../database-factory');
-const {rateLimiter} = require('../util')
+const { dynamo } = require('../database-factory');
+const { rateLimiter } = require('../util');
 
 /**********************
  * READ *
  **********************/
-router.get('', async function(req, res) {
+router.get('', async function (req, res) {
   //const result = await getUser();
   const { currentAppUser } = req ?? {};
-  const {jobLinks, id, isActive, owner, ...rest} = currentAppUser ?? {};
+  const { jobLinks, id, isActive, owner, ...rest } = currentAppUser ?? {};
   let success = currentAppUser ? true : false;
-  
+
   res.json({
     success,
     response: success ? rest : 'There was an error retrieving the user'
   });
 });
 
-const getJobLink = (req, res) => {
-  const { currentAppUser }= req ?? {};
-  const { updatedAt, createdAt, owner, ...user } = currentAppUser ?? {};
-  const response = user?.jobLinks?.pop();
-
-  //TODO: update user jobLinks list w/ dynamo
-  res.status(200).json({ success:true, response });
-};
-
 router.get('/jobLink', async (req, res) => {
-  const {currentAppUser} = req ?? {};
-  const {statusCode} = await rateLimiter.rateLimit(currentAppUser.identifier);
+  const { currentAppUser } = req ?? {};
+  const { statusCode } = await rateLimiter.rateLimit(currentAppUser.identifier);
 
-  switch(statusCode){
+  const getJobLink = async (req, res) => {
+    const { currentAppUser } = req ?? {};
+    const { updatedAt, createdAt, ...user } = currentAppUser ?? {};
+    const response = user?.jobLinks?.pop();
+
+    // update job link list
+    await dynamo.putItem(user);
+    res.status(200).json({ success: true, response });
+  };
+
+  switch (statusCode) {
     case 429:
       res.status(statusCode).json({
-        success:false, response: 'You have reached your rate limit' 
+        success: false,
+        response: 'You have reached your rate limit'
       });
       break;
     case 500:
       res.status(statusCode).json({
-        success:false, response: 'The resource is unavailable at this time' 
+        success: false,
+        response: 'The resource is unavailable at this time'
       });
       break;
     case 200:
@@ -49,65 +52,49 @@ router.get('/jobLink', async (req, res) => {
 });
 
 /****************************
-* CREATE *
-****************************/
-router.post('', async function(req, res) {
-    const {currentAppUser, OPTIONS} = req ?? {};
-    const newAppUserInfo = {...currentAppUser, ...req.body};
-    delete newAppUserInfo.updatedAt;
-    delete newAppUserInfo.createdAt;
-  
-    let response;
-    let success = true;
-  
-    if(currentAppUser){
-      //TODO: update user with dynamoDB
-    } else {
-  
-      //TODO: verify the user has paid and then add to group
-  
-      // const params = {
-      //   GroupName: 'paid-customer', //your confirmed user gets added to this group
-      //   UserPoolId,  
-      //   Username: username
-      // };
-  
-      // await cognitoIdentityServiceProvider.adminAddUserToGroup(params, function(err, data) {
-  
-      //   if (err) {
-      //       callback(err) // uh oh, an error 
-      //   }
-  
-      //   callback(null, event); // yay! success
-      // });
-  
-  
-      // const createQuery = ``;
-      //   //then create new user
-      // (await API.graphql({
-      //   query: createQuery,
-      //   authMode: 'AMAZON_COGNITO_USER_POOLS'
-      // }))?.data?.getUser;
-    }
-  
-    res.json({success, response});
+ * CREATE *
+ ****************************/
+router.post('', async function (req, res) {
+  const { currentAppUser, OPTIONS } = req ?? {};
+  const newAppUserInfo = { ...currentAppUser, ...req.body };
+  delete newAppUserInfo.updatedAt;
+  delete newAppUserInfo.createdAt;
+
+  let response;
+  let success = true;
+
+  if (currentAppUser) {
+    //TODO: update user with dynamoDB
+  } else {
+    //TODO: verify the user has paid and then add to group
+    // const params = {
+    //   GroupName: 'paid-customer', //your confirmed user gets added to this group
+    //   UserPoolId,
+    //   Username: username
+    // };
+    // await cognitoIdentityServiceProvider.adminAddUserToGroup(params, function(err, data) {
+    //   if (err) {
+    //       callback(err) // uh oh, an error
+    //   }
+    //   callback(null, event); // yay! success
+    // });
+    // const createQuery = ``;
+    //   //then create new user
+    // (await API.graphql({
+    //   query: createQuery,
+    //   authMode: 'AMAZON_COGNITO_USER_POOLS'
+    // }))?.data?.getUser;
+  }
+
+  res.json({ success, response });
 });
 
+/****************************
+ * UPDATE *
+ ****************************/
 
 /****************************
-* UPDATE *
-****************************/
-
-
-
-
-
-
-/****************************
-* DELETE *
-****************************/
-
-
-
+ * DELETE *
+ ****************************/
 
 module.exports = router;
